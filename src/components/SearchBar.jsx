d596950856f04f3c1cc81829ef6d745ead8a1ee7
Emma Lowe <38239741+emmalowe@users.noreply.github.com>
2025-05-08 19:43:49 +0100

    Refactor sort method selection as radio buttons

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,39 +2,21 @@ import React from 'react';
 import { BusinessList } from './BusinessList';
 import styles from './styles/SearchBar.module.css';
 
-let sortMethod='best_match';
-
-function handleSortTypeSelect(event) {
-    const elements = event.target.parentNode.querySelectorAll('p');
-    elements.forEach((element) => {
-        element.style.fontWeight = 'normal';
-    })
-    event.target.style.fontWeight = 'bold';
-    sortMethod = event.target.id;
-}
-
-function SortSelection() {
-    return (
-        <div className={styles.sortType}>
-            <p style={{'fontWeight': 'bold'}} id='best_match' onClick={handleSortTypeSelect}>Best Match</p>
-            <p id='rating' onClick={handleSortTypeSelect}>Highest Rated</p>
-            <p id='review_count' onClick={handleSortTypeSelect}>Most Reviewed</p>
-        </div>
-    )
-}
-
-function handleSubmit(event) {
-    
-}
-
 export function SearchBar() {
     return (
         <div>
-            <SortSelection />
-            <form action='#' onSubmit={handleSubmit}>
-                <input className={styles.input} name='term' type='text' placeholder='Search Businesses' />
-                <input className={styles.input} name='location' type='text' placeholder='Where?' />
-                <button className={styles.button} type='submit'>Let's go</button>
+            <form action='#'>
+                <div>
+                    <input className={styles.radioHidden} type="radio" id="option_1" name="choice" defaultChecked/>
+                    <label className={styles.radioLabel} for="option_1" value='best_match'>Best Match</label>
+                    <input className={styles.radioHidden} type="radio" id="option_2" name="choice"/>
+                    <label className={styles.radioLabel} for="option_2" value='rating'>Highest Rated</label>
+                    <input className={styles.radioHidden} type="radio" id="option_3" name="choice"/>
+                    <label className={styles.radioLabel} for="option_3" value='review_count'>Most Reviewed</label>
+                </div>
+                <input className={styles.inputText} name='term' type='text' placeholder='Search Businesses' />
+                <input className={styles.inputText} name='location' type='text' placeholder='Where?' />
+                <button className={styles.buttonSubmit} type='submit'>Let's go</button>
             </form>
         </div>
     )
